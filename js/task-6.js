51e37b140cd115e26824faef63af3bf33c71b3cd
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2021-01-17 18:31:04 +0200

    6 done

---
+++ js/task-6.js
@@ -0,0 +1,12 @@
+//Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+import users from './users.js'
+const getUsersWithAge = (users, min, max) => {
+  // твой код
+    return users.filter(user => user.age < max  && user.age > min)
+};
+
+console.log(getUsersWithAge(users, 20, 30));
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
