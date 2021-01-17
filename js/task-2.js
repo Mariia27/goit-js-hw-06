1d1bf55f804ca46ddec8140b9b3c44a63de42096
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2021-01-17 17:20:26 +0200

    task 1-4 done

--- js/task-2.js
+++ js/task-2.js
@@ -2,25 +2,8 @@
 import users from './users.js'
 const getUsersWithEyeColor = (users, color) => {
   // твой код
-    return users.filter(({eyeColor}) => eyeColor === color).map(user => user.name);
+    return users.filter(({eyeColor}) => eyeColor === color);
 };
-//users.filter(user => Object.values(user) === color)//user.color === color);
+
 console.log(getUsersWithEyeColor(users, 'blue'));
 // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
-
-// const users = [
-//   { name: 'Mango', isActive: true },
-//   { name: 'Poly', isActive: false },
-//   { name: 'Ajax', isActive: true },
-//   { name: 'Chelsey', isActive: false },
-// ];
-
-// // Для каждого элемента коллекции (user) проверим поле isActive.
-// // Если оно true, то текущий элемент (user) будет записан в результирующий массив.
-// const activeUsers = users.filter(user => user.isActive);
-// console.log(activeUsers);
-
-// // Для каждого элемента коллекции (user) проверим поле isActive.
-// // Если оно false, то текущий элемент (user) будет записан в результирующий массив.
-// const inactiveUsers = users.filter(user => !user.isActive);
-// console.log(inactiveUsers);
--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@
 import users from './users.js'
 const getUsersWithGender = (users, gender) => {
   // твой код
- return users.filter(user => user.gender === gender).map(user=>user.name)
+  return users.filter(user => user.gender === gender).map(user => user.name);
   
 };
 
